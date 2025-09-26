81ca47ea3501619bddc6fd5e604557a161c151e4
Manoj Kumar Yadav <manojkryadav95@gmail.com>
2025-09-26 13:39:32 +0530

    Initital commit

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+        $(function () {
+            $('#defaultCountdown').countdown({until: new Date(2025, 6 - 1, 28, 23)}); // year, month, date, hour
+        });
+});		
+
